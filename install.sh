621ea0890c26d159160194dce110cb9ebb8dc65c
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-04-26 02:10:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt install python
 sudo apt install wine -y 
 wget https://www.python.org/ftp/python/3.8.2/python-3.8.2.exe
 sudo wine python-3.8.2.exe
+wget https://github.com/mhammond/pywin32/releases/download/b227/pywin32-227.win32-py3.8.exe
+sudo wine pywin32-227.win32-py3.8.exe
 sudo wine /root/.wine/drive_c/users/root/Local\ Settings/Application\ Data/Programs/Python/Python38-32/python.exe -m pip install pynput==1.6.8
-sudo wine /root/.wine/drive_c/users/root/Local\ Settings/Application\ Data/Programs/Python/Python38-32/python.exe -m pip install win32gui
 sudo wine /root/.wine/drive_c/users/root/Local\ Settings/Application\ Data/Programs/Python/Python38-32/python.exe -m pip install pyinstaller
